0585336951bacbbad7b7633b28314157129b9953
Tatiana <lulu.kep@yandex.ru>
2022-09-04 12:31:50 +0400

    Part1

--- ArrayString/Program.cs
+++ ArrayString/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string[] FullArray = { "hello", "2", "world", ":-)" };
+
+int j = 0;
+for (int i = 0; i < FullArray.Length; i++)
+{
+    if (FullArray[i].Length <= 3 ) j++;
+}
